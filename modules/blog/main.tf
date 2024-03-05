9b44f6d2b7feedd325b39ed85972af69b33a4344
tamaradesmith <tsmith@nautilusdigital.com>
2024-03-05 09:56:16 -0800

    typo three

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -60,7 +60,7 @@ module "blog_alb" {
 
   vpc_id         = module.blog_vpc.vpc_id
   subnets        = module.blog_vpc.public_subnets
-  security_group = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   # Security Group
   security_group_ingress_rules = module.blog_sg.security_group_id
